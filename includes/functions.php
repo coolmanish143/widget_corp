ddbc34f4aa36ac2375ff28056cb960c6362082fa
coolmanish143 <little.punk143@gmail.com>
2017-05-28 18:28:44 +0545

    Using Site Navigation to Choose Content
    1)Adding pages to the navigation subjects...
    2)Refactoring the navigation
    3) Selecting pages from the navigation
    4)Highlighting the current page
    5)Moving the navigation to a function
    6) Moving the navigation to a function
    7)Refactoring the page selection

--- includes/functions.php
+++ includes/functions.php
@@ -12,7 +12,7 @@
 	  global $connection;
 	   $query = "select * ";
 	  $query.=  "from subjects ";
-	  $query .=  "where visible= 1 ";
+	//  $query .=  "where visible= 1 ";
 	  $query .=  "order by position asc";
 	   $subject_set= mysqli_query($connection,$query);
 		  confirm_query($subject_set);
@@ -21,15 +21,137 @@
   
    function find_pages_for_subjects($subject_id)
    {
+	   
 	   global $connection;
+	   $safe_subject_id=mysqli_real_escape_string($connection,$subject_id);
+	  
 	    $query = "select * ";
 	  $query.=  "from pages ";
 	  $query .=  "where visible= 1 ";
-	   $query .="AND subject_id ={$subject_id} ";
+	   $query .="AND subject_id ={$safe_subject_id} ";
 	  $query .=  "order by position asc";
 	   $page_set= mysqli_query($connection,$query);
 		  confirm_query($page_set);
 		  return $page_set;
+	   
+   }
+    function find_subject_by_id($subject_id)
+	{
+		global $connection;
+$safe_subject_id=mysqli_real_escape_string($connection,$subject_id);
+	   $query = "select * ";
+	  $query.=  "from subjects ";
+	  $query .=  "where id={$safe_subject_id} ";
+	  $query .=  "LIMIT 1";
+	   $subject_set= mysqli_query($connection,$query);
+	 confirm_query($subject_set);
+	
+if($subject=mysqli_fetch_assoc($subject_set))
+{
+		  return $subject;
+  
+	}
+	else{
+		return null;
+	}
+	}
+	 function find_page_by_id($page_id)
+	 	{
+		global $connection;
+ $safe_page_id=mysqli_real_escape_string($connection,$page_id);
+	   $query = "select * ";
+	  $query.=  "from pages ";
+	  $query .=  "where id={$safe_page_id} ";
+	  $query .=  "LIMIT 1";
+	   $page_set= mysqli_query($connection,$query);
+	 confirm_query($page_set);
+	
+if($page=mysqli_fetch_assoc($page_set))
+{
+		  return $page;
+  
+	}
+	else{
+		return null;
+	}
+	}
+	 
+	  
+function find_selected_page()
+{
+	global $current_subject;
+	global $current_page;
+	
+	   if(isset($_GET["subject"]))
+	   {
+		   $current_subject=find_subject_by_id($_GET["subject"]);
+		   $current_page=null;
+	   }
+		elseif(isset($_GET["page"]))
+		{
+			
+			$current_subject=null;
+		   $current_page=find_page_by_id($_GET["page"]);
+		   
+		   
+		}
+		else
+		{
+			 
+			 $current_subject=null;
+			  $current_page=null;
+		}
+}	
+ //navigation takeds 2 arguments
+   // -the current subject array or null
+   // -the current page array or null
+   
+   function navigation($subject_array,$page_array)
+   {
+	   $output =" <ul class=\"subjects\">";
+       $subject_set=find_all_subjects();
+	 while($subject = mysqli_fetch_assoc($subject_set))
+	 {
+            	 $output .= "<li" ;
+				 if($subject_array && $subject["id"] == $subject_array["id"])
+				 {
+				 $output .=" class=\"selected\"";
+				 }
+				   $output .= ">";
+	 $output .= "<a href=\"manage_content.php?subject=";
+	  $output .= urlencode($subject["id"]);
+	   $output .= "\">";
+	   $output .=  $subject["menu_name"];
+	    $output .= "</a>";
+	 $page_set= find_pages_for_subjects($subject["id"]);	      
+      $output .= "<ul class=\"pages\">";
+		   
+	 while($page= mysqli_fetch_assoc($page_set))
+	 {
+            	  $output .="<li" ;
+				 if($page_array && $page["id"] == $page_array["id"])
+				 {
+				 $output .=  " class=\"selected\"";
+				 }
+				 $output .= ">";
+	 $output .="<a href=\"manage_content.php?page=";
+	 $output .= urlencode($page["id"]);
+	  $output .= "\">";
+	  $output .=  $page["menu_name"];
+     $output .= "</a></li>";
+	 }
+	
+	   mysqli_free_result($page_set);
+	 
+	  
+	 
+	 $output .=  "</ul></li>";
+
+	 }
+	
+	        mysqli_free_result($subject_set);
+	   $output .= "</ul>";
+	   return $output;
 	   
    }
 ?>
